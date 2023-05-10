057ae343910d9971503b986ad524c5c3263fb9d6
filipepeixoto3 <67834943+filipepeixoto3@users.noreply.github.com>
2023-05-10 15:39:13 +0100

    10/05

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket, json
 from TYPES import *
 
+R = 
+dicionario = {}
 
 def start_server(server_address, server_port):
     # Create an IPv6 socket
@@ -11,8 +13,15 @@ def start_server(server_address, server_port):
 
     while True:
         # Receive data and client address
-        data, address = sock.recvfrom(1024)
+        data = sock.recvfrom(1024)
         data = json.loads(data)
+        dicionario[data[FIELD_IP]]=data
+        for ip1 in dicionario:
+            x = dicionario[ip1][FIELD_POS_X]
+            y = dicionario[ip1][FIELD_POS_Y]
+            for ip2 in dicionario:
+
+
         print(data[FIELD_NAME])
 
 server_address = "2001:690:2280:820::3" 

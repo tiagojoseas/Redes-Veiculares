1258075db3aba4b29d42c9a185709e836b28bfe5
filipepeixoto3 <filipe.8065@gmail.com>
2023-05-10 17:48:37 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 import socket, json
+from datetime import datetime
 from TYPES import *
 
 rsu_address = "2001:690:2280:820::2" 
 rsu_port = 3000 
 
-R1 = 15 #15 pixeis = 10 metros
-R2 = 45
+R = 15 #15 pixeis = 10 metros
 MAX_CARS = 3
 dicionario = {}
 
@@ -20,7 +20,7 @@ def start_server(server_address, server_port):
         data = sock.recvfrom(1024)
         data = json.loads(data)
         dicionario[data[FIELD_IP]] = data
-
+        denm_array = []
         for ip1 in dicionario:
             count_cars = 0
             x1 = dicionario[ip1][FIELD_POS_X]
@@ -30,21 +30,29 @@ def start_server(server_address, server_port):
                     x2 = dicionario[ip2][FIELD_POS_X]
                     y2 = dicionario[ip2][FIELD_POS_Y]
                     dist = ((x2-x1)**2+(y2-y1)**2)**(1/2)
-                    if dist < R1:
+                    if dist < R:
                         count_cars+=count_cars
             if count_cars >= MAX_CARS:
-                msg_denm = {
-                    FIELD_ORIGIN: "2001:690:2280:820::3",
-                    FIELD_DEST: "area",
-                    FIELD_EPICENTER_X: x1,
-                    FIELD_EPICENTER_Y: y1,
-                    FIELD_RADIUS_S : R1,
-                    FIELD_RADIUS_B : R2,
-                    FIELD_NAME: "servidor",
-                    FIELD_TYPE_MSG: DENM_MSG, 
-                    DENM_TYPE: TRAFFIC_JAM
-                }
-                sock.sendto(msg_denm.encode(), (rsu_address, rsu_port))
+                discard = False
+                for demn in denm_array:
+                    if demn[FIELD_TIMESTAMP]<60 and (((demn[FIELD_EPICENTER_X]-x1)**2+(demn[FIELD_EPICENTER_Y]-y1)**2)**(1/2)) < (2*R/3):
+                        discard = True
+                        break
+                if not discard:
+                    msg_denm = {
+                            FIELD_ORIGIN: "2001:690:2280:820::3",
+                            FIELD_DEST: "area",
+                            FIELD_EPICENTER_X: x1,
+                            FIELD_EPICENTER_Y: y1,
+                            FIELD_RADIUS_S : R,
+                            FIELD_RADIUS_B : 3*R,
+                            FIELD_NAME: "servidor",
+                            FIELD_TYPE_MSG: DENM_MSG, 
+                            DENM_TYPE: TRAFFIC_JAM,
+                            FIELD_TIMESTAMP: datetime.now()
+                        }
+                    denm_array.append(msg_denm)
+                    sock.sendto(msg_denm.encode(), (rsu_address, rsu_port))
 
                         
 

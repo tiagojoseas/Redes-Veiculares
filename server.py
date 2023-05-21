616cf90e6c39b15ab6bba05e1e045b7c3d791201
filipepeixoto3 <filipe.8065@gmail.com>
2023-05-21 20:18:27 +0100

    traffic_jam operational

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ rsu_port = 9999
 server_address = "2001:690:2280:820::3"
 server_port = 9999 
 
-R = 100
+R = 150
 MAX_CARS = 2
 data_storage = {}
 denm_dict = {}
@@ -33,35 +33,34 @@ def start_server(server_address, server_port):
             print("SERVER >> New car:",data[FIELD_NAME])
 
         data_storage[data[FIELD_ORIGIN]] = data
-        
         car_conn_time[data[FIELD_ORIGIN]] = datetime.timestamp(datetime.now())
-        #if data[FIELD_TYPE_MSG] == CAM_MSG:
-	        #print("[CAM]",data[FIELD_NAME])
 
-        
         for ip1 in data_storage:
             count_cars = 1
             x1 = data_storage[ip1][FIELD_POS_X]
             y1 = data_storage[ip1][FIELD_POS_Y]
 
             for ip2 in data_storage:    #conta o numero de carro a uma distancia < que R do ip1
+                
                 if ip1 != ip2:
                     x2 = data_storage[ip2][FIELD_POS_X]
                     y2 = data_storage[ip2][FIELD_POS_Y]
                     dist = ((x2-x1)**2+(y2-y1)**2)**(1/2)
+
                     if dist < R:
                         count_cars+=1
+
 
             if count_cars >= MAX_CARS:
                 #try: denm_dict[ip1]
                 #except: denm_dict[ip1] = [0,0,0]
 
-                #if denm_dict:
-                #    for key in denm_dict:
-                #        if(key != ip1):
-                #            if ((denm_dict[key][FIELD_EPICENTER_X]-x1)**2+(denm_dict[key][FIELD_EPICENTER_Y]-y1)**2)**(1/2) < (R/3) and datetime.timestamp(datetime.now())-car_conn_time[key] < 5 :
-                #                discard = True
-                #                break
+                if denm_dict:
+                    for key in denm_dict:
+                        if(key != ip1):
+                            if ((denm_dict[key][FIELD_EPICENTER_X]-x1)**2+(denm_dict[key][FIELD_EPICENTER_Y]-y1)**2)**(1/2) < (R/3) and datetime.timestamp(datetime.now())-car_conn_time[key] < 30 :
+                                discard = True
+                                break
                 
                 
                 if not discard:
